b21850c8cddd2e634e2c0a9cc1ca132a9d23cc42
SiriKrishna <sirikrishnachowdary2002@gmail.com>
2021-07-04 20:26:40 +0530

    lists added

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RestApp',
 ]
 
 MIDDLEWARE = [
@@ -119,12 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static"),
-    
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'RestApp/static/images/')
